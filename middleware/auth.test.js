074439a80ed51a030033d37f0df9d44abbd07c1b
Kristen Krofchik <k.krofchik@gmail.com>
2021-04-24 21:00:13 -0500

    changes to auth tests

--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -5,6 +5,7 @@ const { UnauthorizedError } = require("../expressError");
 const {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin
 } = require("./auth");
 
 
@@ -78,3 +79,26 @@ describe("ensureLoggedIn", function () {
     ensureLoggedIn(req, res, next);
   });
 });
+
+describe("ensureAdmin", function() {
+  test('works', function () {
+    expect.assertions(1);
+    const req = {};
+    const res = { locals: {user: {username: "test", is_admin: true}}};
+    const next = function (err) {
+      expect(err).toBeFalsy();
+    };
+    ensureAdmin(req, res, next);
+    });
+  
+  test("unauth if not admin", function () {
+    expect.assertions(1);
+    const req = {};
+    const res = { locals: {user: {username: "test", is_admin: false}}};
+    const next = function (err) {
+        expect(err instanceof UnauthorizedError).toBeTruthy();
+      };
+      ensureAdmin(req, res, next);
+    });
+});
+
